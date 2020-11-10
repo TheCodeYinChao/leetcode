84734b9792a566d63021510f58c8333bfa92c033
TheCodeYinChao <292306404@qq.com>
2020-11-10 18:38:06 +0800

    平衡二查树

---
+++ src/main/java/cn/leetcode/basestructure/redblacktree/BalanceTwoTree.java
@@ -0,0 +1,16 @@
+package cn.leetcode.basestructure.redblacktree;
+
+/**
+ * dsc: BalanceTwoTree 平衡二叉树
+ * 形成条件
+ *  1 左右两个子树的高度差的绝对值不超过1
+ *  2 并且左右两个子树都是一棵平衡二叉树
+ * 复杂度
+ *  插入，查找，删除的时间复杂度最好情况和最坏情况都维持在O(logN)
+ * date: 2020/11/10 16:39
+ * author: zyc
+ */
+public class BalanceTwoTree {
+
+
+}
