875b101cdfd5d80c18a5ae980b63595d5dc8cd37
TheCodeYinChao <292306404@qq.com>
2020-11-18 19:23:14 +0800

    修改

--- src/main/java/cn/leetcode/basestructure/redblacktree/Tree.java
+++ src/main/java/cn/leetcode/basestructure/redblacktree/Tree.java
@@ -7,16 +7,25 @@ package cn.leetcode.basestructure.redblacktree;
  * @description :
  */
 public class Tree {
+    private Node root;
+
+    public Tree(Node root) {
+        this.root = root;
+    }
+
     public static void main(String[] args) {
-//        int [] array = {4,3,2,8,1,9,12,53};
+        int [] array = {4,3,2,8,1,9,12,53};
 //        int [] array = {4,3,2};
-        int [] array = {4,9,10};
+//        int [] array = {4,9,10};
         Node root = new Node(array[0]);
-        Tree t = new Tree();
+        Tree t = new Tree(root);
         for (int i = 1; i < array.length; i++) {
             t.add(root,array[i]);
         }
         t.println(getroot(root));
+
+
+
 
     }
 
@@ -51,8 +60,6 @@ public class Tree {
             if(left == null){
                 node.left = n;
                 n.parent = node;
-                if(val==2)
-                rRotate(n);
             }else{
                 add(left,val);
             }
@@ -62,17 +69,61 @@ public class Tree {
             if(right == null){
                 node.right = n;
                 n.parent = node;
-                if(val==10)
-                    lRotate(n);
             }else{
                 add(right,val);
             }
         }
+
+        if(this.rightHeigh(root)-this.leftHeigh(root)>1) {
+            this.lRotate(n);
+        }
+        if(this.leftHeigh(root)-this.rightHeigh(root)>1) {
+            this.rRotate(n);
+        }
+        //左子树高>右子树高===>右旋转
+        if(this.leftHeigh(root)-this.rightHeigh(root)>1) {
+            if(n.parent!=null&&leftHeigh(n.parent)<rightHeigh(n.parent)) {
+                lRotate(n.parent);
+            }
+            this.rRotate(n.parent.parent);
+        }
+        //右子树高》左子树高---》左旋转
+        if(this.rightHeigh(root)-this.leftHeigh(root)>1) {
+            if(n.parent==null&&leftHeigh(n.parent)>rightHeigh(n.parent)) {
+                rRotate(n.parent);
+            }
+            lRotate(n.parent.parent);
+        }
+    }
+
+    //计算左子树的高度
+    public int leftHeigh(Node node) {
+        if(node.left==null) {
+            return 0;
+        }
+        return getHeight(node.left);
+    }
+    //计算右子树的高度
+    public int rightHeigh(Node node) {
+        if(node.right==null) {
+            return 0;
+        }
+        return getHeight(node.right);
+    }
+    public int getHeight(Node node){
+        if(node == null){
+            return 0;
+        }
+        int i = getHeight(node.left);
+        int j = getHeight(node.right);
+        return (i<j)? j+1:i+1;
     }
 
 
+
+
     /**
-     * 左旋
+     * 左旋 错的
      * @param node
      */
     public void lRotate(Node node){
@@ -82,6 +133,7 @@ public class Tree {
             node.parent.parent = parent2;
         }else {
             node.parent.parent = null;
+            root = node.parent;
         }
         parent1.right = null;
         parent1.parent = node.parent;
@@ -89,7 +141,7 @@ public class Tree {
     }
 
     /**
-     * 右旋
+     * 右旋 错的
      * @param node
      */
     public void rRotate(Node node){
@@ -99,6 +151,7 @@ public class Tree {
             node.parent.parent = parent2;
         }else {
             node.parent.parent = null;
+            root = node.parent;
         }
         parent1.left = null;
         parent1.parent = node.parent;
